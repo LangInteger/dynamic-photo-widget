24a97d2555499d0bbdb7d7f3030a8877bfe88814
liulang <709091988@qq.com>
2023-08-15 23:05:29 +0800

    feat: add load from cloud icon, topic and date

--- LangWidget/LangWidget.swift
+++ LangWidget/LangWidget.swift
@@ -29,21 +29,21 @@ private struct Provider: TimelineProvider {
     // MARK: TimelineProvider
 
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+        SimpleEntry(date: Date(), topic: "Find Something New", description: "", pictureDate: "2023-08")
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
-        let entry = SimpleEntry(date: Date())
+        let entry = SimpleEntry(date: Date(), topic: "Find Something New", description: "", pictureDate: "2023-08")
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
-        loadImageData { imageData, isCached, description in
+        loadImageData { imageData, isCached, description, topic, pictureDate in
             let currentDate = Date()
             let nextDate = Calendar.current.date(byAdding: .minute, value: 1, to: currentDate)!
             let entries = [
-                SimpleEntry(date: currentDate, imageData: imageData, isCached: isCached, description: description),
-                SimpleEntry(date: nextDate, imageData: imageData, isCached: isCached, description: description)
+                SimpleEntry(date: currentDate, imageData: imageData, isCached: isCached, topic: topic, description: description, pictureDate: pictureDate),
+                SimpleEntry(date: nextDate, imageData: imageData, isCached: isCached, topic: topic, description: description, pictureDate: pictureDate)
             ]
             let timeline = Timeline(entries: entries, policy: .atEnd)
             completion(timeline)
@@ -51,7 +51,7 @@ private struct Provider: TimelineProvider {
     }
     
     // Function to fetch JSON synchronously and get a random link
-    func fetchRandomLinkSync(from url: URL) -> String? {
+    func fetchRandomLinkSync(from url: URL) -> (randomLink: String, counterText: String, pictureDate: String)? {
         guard let data = try? Data(contentsOf: url) else {
             return nil
         }
@@ -71,39 +71,51 @@ private struct Provider: TimelineProvider {
             return nil
         }
         
+        guard let texts = randomizedList.first?.arr.text else {
+            return nil
+        }
+        
         // Generate a random index
         let randomIndex = Int.random(in: 0..<links.count)
         
         // Get the random link
         let randomLink = links[randomIndex]
+        let counterText = texts[randomIndex]
         
-        return randomLink
+        var pictureDate = Self.UNKNOWN_DATE;
+        let parts = randomLink.components(separatedBy: "_")
+        if let firstPart = parts.first {
+            pictureDate = firstPart
+        }
+        
+        return (randomLink, counterText, pictureDate)
     }
     
     private static let metaUrlString = "https://raw.githubusercontent.com/LangInteger/LangInteger.github.io/master/photos/data.json"
     private static let pictureBaseUrl = "https://raw.githubusercontent.com/LangInteger/blog_photos/main/min_photos/"
-    private func loadImageData(completion: @escaping (Data?, Bool, String) -> Void) {
+    private static let UNKNOWN_DATE = "Unknown Date"
+    private func loadImageData(completion: @escaping (Data?, Bool, String, String, String) -> Void) {
         guard let metaUrl = URL(string: Self.metaUrlString) else {
-            completion(nil, false, "fail fetch from meta url")
+            completion(nil, false, "fail fetch from meta url", "", Self.UNKNOWN_DATE)
             return
         }
         
-        guard let randomLink = fetchRandomLinkSync(from: metaUrl) else {
-            completion(nil, false, "fail parse result of meta url")
+        guard let linkResult = fetchRandomLinkSync(from: metaUrl) else {
+            completion(nil, false, "fail parse result of meta url", "", Self.UNKNOWN_DATE)
             return
         }
         
-        let pictureUrl = Self.pictureBaseUrl + randomLink
+        let pictureUrl = Self.pictureBaseUrl + linkResult.randomLink
         
         guard let encodedPictureUrl = pictureUrl.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
               let url = URL(string: encodedPictureUrl) else {
-            completion(nil, false, "illegal random link " + pictureUrl)
+            completion(nil, false, "illegal random link " + pictureUrl, "", Self.UNKNOWN_DATE)
             return
         }
         
         
         if let imageData = getImageFromCache(url: url) {
-            completion(imageData, true, "load from cache")
+            completion(imageData, true, "load from cache", linkResult.counterText, linkResult.pictureDate)
             return
         }
 
@@ -113,7 +125,7 @@ private struct Provider: TimelineProvider {
             .sink { _ in}
                   receiveValue: {
                     Self.cache.setObject($0 as NSData, forKey: url as NSURL)
-                    completion($0, false, "load from cloud")
+                      completion($0, false, "load from cloud", linkResult.counterText, linkResult.pictureDate)
             }
             .store(in: &Self.cancellables)
     }
@@ -124,6 +136,7 @@ private struct Response: Codable {
     struct ListEntry: Codable {
         struct ArrEntry: Codable {
             let link: [String]
+            let text: [String]
         }
         let arr: ArrEntry
     }
@@ -134,18 +147,47 @@ private struct SimpleEntry: TimelineEntry {
     let date: Date
     var imageData: Data?
     var isCached = false
-    var description = ""
+    let topic: String
+    let description: String
+    let pictureDate: String
 }
 
 private struct URLCachedImageWidgetEntryView: View {
+    @Environment(\.widgetFamily) var family: WidgetFamily
     var entry: Provider.Entry
-
     var body: some View {
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.black.opacity(0),.black.opacity(0.4)]), startPoint: UnitPoint(x: 0, y: 0.5), endPoint: UnitPoint(x: 0, y: 1))
+            VStack(alignment: .leading){
+                Spacer()
+                HStack(alignment: .center){
+                    if !entry.isCached {
+                        Image(uiImage: UIImage(named: "cloud")!)
+                            .antialiased(true)
+                            .aspectRatio(contentMode: .fill)
+                            //.frame(width:family == .systemSmall ? 28: 32, height: family == .systemSmall ? 28: 32,alignment: .leading).cornerRadius(family == .systemSmall ? 14: 16)
+                        Spacer().frame(width: family == .systemSmall ? 6: 8, height: 1, alignment: .bottomLeading)
+                    }
+                    // name
+
+                    VStack{
+                        Text(entry.topic).font(family == .systemSmall ? .system(size: 12).weight(.semibold)
+                                               : .system(size: 16).weight(.semibold)).lineLimit(1).foregroundColor(Color.white)
+                        Text("2023-08").font(family == .systemSmall ? .system(size: 12).weight(.semibold)
+                                             : .system(size: 16).weight(.semibold)).lineLimit(1).foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+            }.padding(.all,10)
+        }.background(imageContainer)
+    }
+    
+    @ViewBuilder
+    var imageContainer: some View {
         if let data = entry.imageData {
             URLImageView(data: data)
                 .aspectRatio(contentMode: .fill)
         }
-        Text("\(entry.description)")
     }
 }
 
